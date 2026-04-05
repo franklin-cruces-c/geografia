2684ab9642e6058d9ba4d6a45b12a83ac7b2a0f7
Franklin Cruces <150628856+franklin-cruces-c@users.noreply.github.com>
2026-04-05 11:42:52 +0200

    Add dictionary of exceptions with capitals and coordinates

---
+++ diccionario-excepciones.js
@@ -0,0 +1,13 @@
+window.DICCIONARIO_EXCEPCIONES = {
+    "Åland": { capital: "Mariehamn", lat: 60.0973, lon: 19.9348 },
+    "Islas Feroe": { capital: "Tórshavn", lat: 62.0079, lon: -6.7900 },
+    "Gibraltar": { capital: "Gibraltar", lat: 36.1408, lon: -5.3536 },
+    "Jersey": { capital: "Saint Helier", lat: 49.1868, lon: -2.1066 },
+    "Guernsey": { capital: "Saint Peter Port", lat: 49.4550, lon: -2.5369 },
+    "Isla de Man": { capital: "Douglas", lat: 54.1523, lon: -4.4861 },
+    "Svalbard": { capital: "Longyearbyen", lat: 78.2232, lon: 15.6469 },
+    "Abjasia": { capital: "Sujumi", lat: 43.0015, lon: 41.0234 },
+    "Osetia del Sur": { capital: "Tsjinvali", lat: 42.2278, lon: 43.9686 },
+    "Transnistria": { capital: "Tiráspol", lat: 46.8403, lon: 29.6433 },
+    "Artsaj": { capital: "Stepanakert", lat: 39.8156, lon: 46.7528 }
+};
